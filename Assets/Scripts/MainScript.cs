f1e3402e89af619ae7acfa14ade72baf677619de
Joel Eager <jee7291@rit.edu>
2016-04-02 15:50:57 -0400

    Copied over code from jetpack test

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+	public GameObject cardboardObj;
+	public int yAcc;
+	public int zAcc;
+	
+	private Rigidbody cardboardObjRB;
+	
+	void Start() {
+		Cardboard.Create();
+		Cardboard.SDK.OnTrigger += TriggerPulled;
+		
+		cardboardObjRB = cardboardObj.GetComponent<Rigidbody>();
+	}
+
+	void TriggerPulled() {
+		Vector3 acc = new Vector3(0, 0, zAcc);
+		acc = transform.rotation * acc;
+		acc.y = yAcc;
+		Debug.Log(acc);
+		cardboardObjRB.AddForce(acc);
+	}
+	
+	void Update() {
+		if (cardboardObj.transform.position.y < 1) {
+			cardboardObj.transform.position = new Vector3(0, 3, 0);
+		}
+	}
+}
